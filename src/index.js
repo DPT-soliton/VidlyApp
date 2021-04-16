327b26602acd0314c70b21fde593fc8080f8e514
dtorrefiel@csyoutsourcing.com <dtorrefiel@csyoutsourcing.com>
2021-04-16 09:23:12 +0800

    commented out App render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -9,7 +9,7 @@ import Movies from "./components/movies";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     <Movies />
   </React.StrictMode>,
   document.getElementById("root")
